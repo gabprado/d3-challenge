523c9d07e54cdf997c0cbad09051a4a800920c3c
gabprado <gabprado@hotmail.com>
2020-03-29 23:38:55 -0500

    add tool tip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 const svgWidth = 960
 const svgHeight = 500
 let margin = {
-  top: 40,
-  right: 90,
-  bottom: 80,
+  top: 20,
+  right: 40,
+  bottom: 100,
   left: 100
 }
+
 let width = svgWidth - margin.left - margin.right
 let height = svgHeight - margin.top - margin.bottom
 let svg = d3
@@ -16,9 +17,10 @@ let svg = d3
 let chartGroup = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
-
 let xSelection = "poverty"
-let ySelection = "healthcare"
+let xTipLabel = "In Poverty (%):"
+let ySelection = "obesity"
+let yTipLabel = "Obese (%):"
 
 function xScale(data, xSelection) {
     let xLinearScale = d3
@@ -78,6 +80,43 @@ function renderCirclesTextGroup(circlesTextGroup, newXScale, xSelection, newYSca
     return circlesTextGroup
 }
 
+function renderToolTip(xSelection, xTipLabel, ySelection, yTipLabel, circlesGroup){
+    let toolTip = d3
+        .tip()
+        .attr("class", "d3-tip")
+        .html(d => `${d.state}<br>
+                    ${xTipLabel}${d[xSelection]}<br>
+                    ${yTipLabel}${d[ySelection]}<br>`
+        )    
+        circlesGroup.call(toolTip)
+        circlesGroup
+            .on("mouseover", function(data) {
+                toolTip.show(data, this)
+            })
+            .on("mouseout", function(data) {
+                toolTip.hide(data)
+            })
+    return circlesGroup
+}
+
+function refreshObjects(xLinearScale, xSelection, xTipLabel, yLinearScale, ySelection, yTipLabel, circlesGroup, circlesTextGroup) {
+    circlesGroup = renderCircles(
+        circlesGroup,
+        xLinearScale,
+        xSelection,
+        yLinearScale,
+        ySelection
+    )
+    circlesTextGroup = renderCirclesTextGroup(
+        circlesTextGroup,
+        xLinearScale,
+        xSelection,
+        yLinearScale,
+        ySelection
+    )
+    circlesGroup = renderToolTip(xSelection, xTipLabel, ySelection, yTipLabel, circlesGroup)
+}
+
 function renderPlot() {
     d3.csv("assets/data/data.csv").then(data => {
         let xLinearScale = xScale(data, xSelection)
@@ -101,8 +140,9 @@ function renderPlot() {
             .attr("cx", d => xLinearScale(d[xSelection]))
             .attr("cy", d => yLinearScale(d[ySelection]))
             .attr("r", 12)
-            .attr("fill", "red")
-            .attr("opacity", ".3")
+            .attr("fill", "purple")
+            .attr("stroke", "black")
+            .attr("opacity", ".5")
         let circlesTextGroup = chartGroup
             .selectAll()
             .data(data)
@@ -110,15 +150,17 @@ function renderPlot() {
             .append("text")
             .attr("x", d => xLinearScale(d[xSelection]))
             .attr("y", d => yLinearScale(d[ySelection]))
-            .style("font-size", "10px")
+            .attr("dy", 3)
+            .style("font-size", "7px")
             .style("fill", "black")
-            .style("text-acnhor", "middle")
+            .style("stroke", "white")
+            .style("text-anchor", "middle")
             .text(d => d.abbr)
         let xLabels = chartGroup
             .append("g")
             .attr("transform", `translate(${width / 2}, 
                     ${height + 20})`)
-        let yLables = chartGroup
+        let yLabels = chartGroup
             .append("g")
             .attr("transform", "rotate(-90)")
         xLabels
@@ -127,72 +169,87 @@ function renderPlot() {
             .attr("y", 20)
             .attr("value", "poverty")
             .classed("active", true)
-            .text("In Poverty")
+            .text("In Poverty (%)")
         xLabels
             .append("text")
             .attr("x", 0)
             .attr("y", 40)
             .attr("value", "age")
             .classed("inactive", true)
-            .text("Age")
-        yLables
+            .text("Age (Median)")
+        xLabels
+            .append("text")
+            .attr("x", 0)
+            .attr("y", 60)
+            .attr("value", "income")
+            .classed("inactive", true)
+            .text("Income (Median)")
+        yLabels
             .append("text")
             .attr("y", 0 - margin.left)
             .attr("x", 0 - height / 2)
             .attr("dy", "1em")
-            .attr("value", "healthcare")
+            .attr("value", "obesity")
             .classed("active", true)
-            .text("Healthcare")
-        yLables
+            .text("Obese (%)")
+        yLabels
             .append("text")
             .attr("y", 20 - margin.left)
             .attr("x", 0 - height / 2)
             .attr("dy", "1em")
             .attr("value", "smokes")
             .classed("inactive", true)
-            .text("Smokes")
-        
+            .text("Smokes (%)")
+        yLabels
+            .append("text")
+            .attr("y", 40 - margin.left)
+            .attr("x", 0 - height / 2)
+            .attr("dy", "1em")
+            .attr("value", "noHealthInsurance")
+            .classed("inactive", true)
+            .text("Lacks Health Insurance (%)")
+        circlesGroup = renderToolTip(xSelection, xTipLabel, ySelection, yTipLabel, circlesGroup)
+        //click event handlers
         xLabels.selectAll("text").on("click", function() {
             let xValue = d3.select(this).attr("value")
             if (xValue !== xSelection) {
+                xTipLabel = (d3.select(this).text()) + ": "
+                xLabels.selectAll("text").classed("inactive", true).classed("active", false)
+                d3.select(this).classed("active", true).classed("inactive", false)
                 xSelection = xValue
                 xLinearScale = xScale(data, xSelection)
                 xAxis = renderXAxes(xLinearScale, xAxis)
-                circlesGroup = renderCircles(
-                    circlesGroup,
+                refreshObjects(
                     xLinearScale,
-                    xSelection,
-                    yLinearScale,
-                    ySelection
+                    xSelection, 
+                    xTipLabel, 
+                    yLinearScale, 
+                    ySelection, 
+                    yTipLabel, 
+                    circlesGroup, 
+                    circlesTextGroup
                 )
-                circlesTextGroup = renderCirclesTextGroup(
-                    circlesTextGroup,
-                    xLinearScale,
-                    xSelection,
-                    yLinearScale,
-                    ySelection
-                )
+
             }
         })
-        yLables.selectAll("text").on("click", function() {
+        yLabels.selectAll("text").on("click", function() {
             let yValue = d3.select(this).attr("value")
             if (yValue !== ySelection) {
+                yTipLabel = (d3.select(this).text()) + ": "
+                yLabels.selectAll("text").classed("inactive", true).classed("active", false)
+                d3.select(this).classed("active", true).classed("inactive", false)
                 ySelection = yValue
                 yLinearScale = yScale(data, ySelection)
                 yAxis = renderYAxes(yLinearScale, yAxis)
-                circlesGroup = renderCircles(
-                    circlesGroup,
+                refreshObjects(
                     xLinearScale,
-                    xSelection,
-                    yLinearScale,
-                    ySelection
-                )
-                circlesTextGroup = renderCirclesTextGroup(
-                    circlesTextGroup,
-                    xLinearScale,
-                    xSelection,
-                    yLinearScale,
-                    ySelection
+                    xSelection, 
+                    xTipLabel, 
+                    yLinearScale, 
+                    ySelection, 
+                    yTipLabel, 
+                    circlesGroup, 
+                    circlesTextGroup
                 )
             }
         })
